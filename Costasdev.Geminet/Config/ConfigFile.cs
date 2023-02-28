c4e6f36c68d802efdf61e1e037e2af8a438c63f8
Ariel Costas <94913521+arielcostas@users.noreply.github.com>
2023-02-28 11:31:59 +0100

    improvements to certificates
    - Generate serial number
    - Save all in one file
    - More properties for the subject
    
    Closes #1

--- Costasdev.Geminet/Config/ConfigFile.cs
+++ Costasdev.Geminet/Config/ConfigFile.cs
@@ -1,12 +1,16 @@
-﻿using Newtonsoft.Json;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Newtonsoft.Json;
 
 namespace Costasdev.Geminet.Config
 {
     public class ConfigRoot
     {
-        [JsonProperty("certRoot")] public string CertRoot { get; set; }
+        [JsonProperty("certFile")] public string CertFile { get; set; }
 
         [JsonProperty("certPassword")] public string CertPassword { get; set; }
+
+        [JsonProperty("certSubject")] public CertificateSubject CertOptions { get; set; }
 
         [JsonProperty("sites")] public ConfigSite[] Sites { get; set; }
     }
@@ -23,5 +27,55 @@ namespace Costasdev.Geminet.Config
         [JsonProperty("serve")] public string Serve { get; set; }
 
         [JsonProperty("index")] public Boolean? GenerateIndex { get; set; }
+    }
+
+    public class CertificateSubject
+    {
+        [JsonProperty("c")]
+        public string? Country { get; set; }
+        
+        [JsonProperty("o")]
+        public string? Organization { get; set; }
+        
+        [JsonProperty("ou")]
+        public string? OrganizationalUnit { get; set; }
+        
+        [JsonProperty("st")]
+        public string? State { get; set; }
+     
+        [JsonProperty("locality")]
+        public string? Locality { get; set; }
+
+        public override string ToString()
+        {
+            List<String> parts = new();
+            
+            if (!string.IsNullOrEmpty(Organization))
+            {
+                parts.Add($"O={Organization}");
+            }
+            
+            if (!string.IsNullOrEmpty(OrganizationalUnit))
+            {
+                parts.Add($"OU={OrganizationalUnit}");
+            }
+            
+            if (!string.IsNullOrEmpty(Country))
+            {
+                parts.Add($"C={Country}");
+            }
+            
+            if (!string.IsNullOrEmpty(State))
+            {
+                parts.Add($"ST={State}");
+            }
+            
+            if (!string.IsNullOrEmpty(Locality))
+            {
+                parts.Add($"L={Locality}");
+            }
+            
+            return string.Join(", ", parts);
+        }
     }
 }
